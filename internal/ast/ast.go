dfae5eca8e6ac464406d835165d0cbafae3cc4a4
Nick Calibey <nick.calibey@gmail.com>
2020-03-07 12:13:27 -0600

    Finish implementing Parser

--- internal/ast/ast.go
+++ internal/ast/ast.go
@@ -2,6 +2,7 @@ package ast
 
 import (
 	"bytes"
+	"strings"
 
 	"github.com/ncalibey/monkey/internal/token"
 )
@@ -104,7 +105,7 @@ type Identifier struct {
 }
 
 func (i *Identifier) statementNode()       {}
-func (i *Identifier) expressionNode() 	   {}
+func (i *Identifier) expressionNode()      {}
 func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
 func (i *Identifier) String() string       { return i.Value }
 
@@ -128,6 +129,174 @@ func (r *ReturnStatement) String() string {
 	}
 
 	out.WriteString(";")
+
+	return out.String()
+}
+
+// IntegerLiteral represents a integer literal within an AST.
+type IntegerLiteral struct {
+	// Token is the token.INT token.
+	Token token.Token
+	// Value is the integer bound to the literal.
+	Value int64
+}
+
+func (i *IntegerLiteral) expressionNode()      {}
+func (i *IntegerLiteral) TokenLiteral() string { return i.Token.Literal }
+func (i *IntegerLiteral) String() string       { return i.Token.Literal }
+
+// PrefixExpression represents a prefix expression within an AST.
+type PrefixExpression struct {
+	// Token is the token.PREFFIX token.
+	Token token.Token
+	// Operator is the operator within the expression.
+	Operator string
+	// Right is the expression to the right of the operator.
+	Right Expression
+}
+
+func (p *PrefixExpression) expressionNode() 	 {}
+func (p *PrefixExpression) TokenLiteral() string { return p.Token.Literal }
+func (p *PrefixExpression) String() string {
+	var out bytes.Buffer
+
+	out.WriteString("(")
+	out.WriteString(p.Operator)
+	out.WriteString(p.Right.String())
+	out.WriteString(")")
+
+	return out.String()
+}
+
+// InfixExpression represents an infix expression within an AST.
+type InfixExpression struct {
+	Token token.Token
+	// Left is the expression to the left of the operator.
+	Left Expression
+	// Operator is the operator within the expression.
+	Operator string
+	// Right is the expression to the right of the operator.
+	Right Expression
+}
+
+func (i *InfixExpression) expressionNode()                                                             {}
+func (i *InfixExpression) TokenLiteral() string { return i.Token.Literal }
+func (i *InfixExpression) String() string {
+	var out bytes.Buffer
+
+	out.WriteString("(")
+	out.WriteString(i.Left.String())
+	out.WriteString(" " + i.Operator + " ")
+	out.WriteString(i.Right.String())
+	out.WriteString(")")
+
+	return out.String()
+}
+
+// Boolean represents a boolean literal within an AST.
+type Boolean struct {
+	Token token.Token
+	// Value is the boolean bound to the literal.
+	Value bool
+}
+
+func (b *Boolean) expressionNode() 		{}
+func (b *Boolean) TokenLiteral() string { return b.Token.Literal }
+func (b *Boolean) String() string 		{ return b.Token.Literal }
+
+// IfExpression represents an if expression within an AST.
+type IfExpression struct {
+	Token token.Token
+	Condition Expression
+	Consequence *BlockStatement
+	Alternative *BlockStatement
+}
+
+func (ie *IfExpression) expressionNode() {}
+func (ie *IfExpression) TokenLiteral() string { return ie.Token.Literal }
+func (ie *IfExpression) String() string {
+	var out bytes.Buffer
+
+	out.WriteString("if")
+	out.WriteString(ie.Condition.String())
+	out.WriteString(" ")
+	out.WriteString(ie.Consequence.String())
+
+	if ie.Alternative != nil {
+		out.WriteString("else ")
+		out.WriteString(ie.Alternative.String())
+	}
+
+	return out.String()
+}
+
+// BlockStatement represents a block statement within an AST.
+type BlockStatement struct {
+	Token token.Token
+	// Statments are the statments within the Block.
+	Statements []Statement
+}
+
+func (b *BlockStatement) statementNode() {}
+func (b *BlockStatement) TokenLiterael() string { return b.Token.Literal }
+func (b *BlockStatement) String() string {
+	var out bytes.Buffer
+
+	for _, s := range b.Statements {
+		out.WriteString(s.String())
+	}
+
+	return out.String()
+}
+
+type FunctionLiteral struct {
+	// The 'fn' token
+	Token token.Token
+	Parameters []*Identifier
+	Body *BlockStatement
+}
+
+func (fl *FunctionLiteral) expressionNode() {}
+func (fl *FunctionLiteral) TokenLiteral() string { return fl.Token.Literal }
+func (fl *FunctionLiteral) String() string {
+	var out bytes.Buffer
+
+	params := []string{}
+	for _, p := range fl.Parameters {
+		params = append(params, p.String())
+	}
+
+	out.WriteString(fl.TokenLiteral())
+	out.WriteString("(")
+	out.WriteString(strings.Join(params, ", "))
+	out.WriteString(") ")
+	out.WriteString(fl.Body.String())
+
+	return out.String()
+}
+
+type CallExpression struct {
+	// The '(' token
+	Token token.Token
+	// Identifier or FunctionLiteral
+	Function Expression
+	Arguments []Expression
+}
+
+func (ce *CallExpression) expressionNode() {}
+func (ce *CallExpression) TokenLiteral() string { return ce.Token.Literal }
+func (ce *CallExpression) String() string {
+	var out bytes.Buffer
+
+	args := []string{}
+	for _, a := range ce.Arguments {
+		args = append(args, a.String())
+	}
+
+	out.WriteString(ce.Function.String())
+	out.WriteString("(")
+	out.WriteString(strings.Join(args, ", "))
+	out.WriteString(")")
 
 	return out.String()
 }
--- internal/repl/repl.go
+++ internal/repl/repl.go
@@ -6,10 +6,24 @@ import (
 	"io"
 
 	"github.com/ncalibey/monkey/internal/lexer"
-	"github.com/ncalibey/monkey/internal/token"
+	"github.com/ncalibey/monkey/internal/parser"
 )
 
-const PROMPT = ">> "
+const (
+	PROMPT = ">> "
+	MONKEY_FACE = `            __,__
+   .--.  .-"     "-.  .--.
+  / .. \/  .-. .-.  \/ .. \
+ | |  '|  /   Y   \  |'  | |
+ | \   \  \ 0 | 0 /  /   / |
+  \ '- ,\.-"""""""-./, -' /
+   ''-' /_   ^ ^   _\ '-''
+       |  \._   _./  |
+       \   \ '~' /   /
+        '._ '-=-' _.'
+           '-----'
+`
+)
 
 // Start begins a monkey REPL session.
 func Start(in io.Reader, out io.Writer) {
@@ -18,16 +32,29 @@ func Start(in io.Reader, out io.Writer) {
 	for {
 		fmt.Printf(PROMPT)
 		scanned := scanner.Scan()
-
 		if !scanned {
 			return
 		}
 
 		line := scanner.Text()
 		l := lexer.New(line)
+		p := parser.New(l)
 
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Printf("%+v\n", tok)
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
 		}
+
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
+	}
+}
+
+func printParserErrors(out io.Writer, errors []string) {
+	io.WriteString(out, MONKEY_FACE)
+	io.WriteString(out, "Woops! We ran into some monkey business here!\n")
+	io.WriteString(out, " parser errors:\n")
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
 	}
 }
